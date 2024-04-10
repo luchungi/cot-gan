a4eb7ee50ba7ed6494d6cebe9275dbe9e3bea2ad
WSL-Lu <lu.chung.i@gmail.com>
2024-04-10 15:41:50 +0800

    Evaluating stylised facts

---
+++ preprocessing.py
@@ -0,0 +1,24 @@
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from gaussianize import Gaussianize
+
+def rolling_window(x, k, sparse=True):
+    """compute rolling windows from timeseries
+
+    Args:
+        x ([2d array]): x contains the time series in the shape (timestep, sample).
+        k ([int]): window length.
+        sparse (bool): Cut off the final windows containing NA. Defaults to True.
+
+    Returns:
+        [3d array]: array of rolling windows in the shape (window, timestep, sample).
+    """
+    out = np.full([k, *x.shape], np.nan)
+    N = len(x)
+    for i in range(k):
+        out[i, :N-i] = x[i:]
+
+    if not sparse:
+        return out
+
+    return out[:, :-(k-1)]
